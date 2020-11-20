7201c838ede2aa836069c37600bc8c7861edc2d1
Prakhar Soni <prakhar.amansoni@gmail.com>
2020-11-20 09:58:18 +0530

    second comit for ocr clone

--- app/src/main/java/com/myappcompany/proprakhar/ocr_letscopy/MainActivity.java
+++ app/src/main/java/com/myappcompany/proprakhar/ocr_letscopy/MainActivity.java
@@ -1,5 +1,6 @@
 package com.myappcompany.proprakhar.ocr_letscopy;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.ActivityNotFoundException;
@@ -7,15 +8,27 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.text.FirebaseVisionText;
+import com.google.firebase.ml.vision.text.FirebaseVisionTextDetector;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private ImageView imageView;
     private Button capture,detect;
     private TextView textView;
+    private Bitmap imageBitmap;
     static final int REQUEST_IMAGE_CAPTURE = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,9 +42,18 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
              dispatchTakePictureIntent();
+             textView.setText("");
+            }
+        });
+        detect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                detectTextFromImage();
             }
         });
     }
+
+
 
 
     private void dispatchTakePictureIntent() {
@@ -47,8 +69,37 @@ public class MainActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
             Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imageBitmap = (Bitmap) extras.get("data");
             imageView.setImageBitmap(imageBitmap);
+        }
+    }
+    private void detectTextFromImage() {
+        FirebaseVisionImage firebaseVisionImage=FirebaseVisionImage.fromBitmap(imageBitmap);
+        FirebaseVisionTextDetector firebaseVisionTextDetector=FirebaseVision.getInstance().getVisionTextDetector();
+        firebaseVisionTextDetector.detectInImage(firebaseVisionImage).addOnSuccessListener(new OnSuccessListener<FirebaseVisionText>() {
+            @Override
+            public void onSuccess(FirebaseVisionText firebaseVisionText) {
+                displayTextFromImage(firebaseVisionText);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(MainActivity.this, "Error: " +e.getMessage(), Toast.LENGTH_SHORT).show();
+                Log.d("Error",e.getMessage());
+            }
+        });
+    }
+
+    private void displayTextFromImage(FirebaseVisionText firebaseVisionText) {
+        List<FirebaseVisionText.Block> blockList=firebaseVisionText.getBlocks();
+        if(blockList.size()==0){
+            Toast.makeText(this, "No text found in Image", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            for(FirebaseVisionText.Block block : firebaseVisionText.getBlocks() ){
+             String text =block.getText();
+             textView.setText(text);
+            }
         }
     }
 }
